2eacb68848759225a9b45810ab3429c8bda0b206
Noa Nissim <87809765+noanissim@users.noreply.github.com>
2021-10-04 20:21:20 +0300

    second commit

--- js/app.js
+++ js/app.js
@@ -20,12 +20,19 @@ var gGame = {
 
 var gClicksTotal
 var gFlagsTotal
+var gIsVictory
 
+var gStartTime
+var gDiff
+var gCurrRecord = Infinity
+var gInterval
+var gLife
+var gHearts
 
 function init() {
     //model
     gBoard = buildBoard()
-    console.table(gBoard)
+    // console.table(gBoard)
     //dom
     renderBoard(gBoard, '.board-container')
 
@@ -36,10 +43,19 @@ function init() {
 
     gClicksTotal = 0
     gFlagsTotal = 0
+    gIsVictory = false
+    gLife = 3
+    gHearts = '❤❤❤'
 
-    setMinesNegsCount(gBoard)
-    renderBoard(gBoard, '.board-container')
-    document.querySelector('h3 span').innerText = gLevel.MINES;
+
+    document.querySelector('.flag span').innerText = gLevel.MINES;
+    document.querySelector('h4').classList.add('hide');
+    document.querySelector('.timer').innerText = `00:00:00`
+    // document.querySelector('.restart').classList.add('hide');
+    document.querySelector('.life span').innerText = gHearts;
+    document.querySelector('.restart span').innerText = '😁'
+
+
 
 }
 
@@ -58,20 +74,31 @@ function buildBoard() {
             }
         }
     }
-    randomBombs(board, gLevel.SIZE, gLevel.MINES)
+
     return board;
 }
 
 
 
-
-
-
-
-
 function cellClicked(elCell, i, j, ev) {
 
+    if (gBoard[i][j].isMarked) {
+        return
+    }
+
+
+    gGame.shownCount++
+    // console.log(gGame.shownCount);
+    if (gGame.shownCount === 1 && gGame.markedCount === 0) {
+        console.log('first click');
+        randomBombs(gBoard, gLevel.SIZE, gLevel.MINES, i, j)
+        setMinesNegsCount(gBoard)
+        renderBoard(gBoard, '.board-container')
+        startTimer()
+
+    }
     gBoard[i][j].isShown = true
+
 
     renderBoard(gBoard, '.board-container')
     //the model td content will be empty, the dom td content is filled
@@ -82,30 +109,46 @@ function cellClicked(elCell, i, j, ev) {
         expandShown(gBoard, elCell, i, j)
     }
 
-    if (gBoard[i][j].isMine) {
+    if (gBoard[i][j].isMine && gLevel.SIZE > 5) {
+        if (gLife === 3) {
+            gLife--
+            gHearts = '❤❤'
+        } else if (gLife === 2) {
+            gLife--
+            gHearts = '❤'
+        } else if (gLife === 1) {
+            gLife--
+            gHearts = ''
+            gIsVictory = false
+            loseGame()
+        }
 
+    } else if (gBoard[i][j].isMine) {
+        gIsVictory = false
+        loseGame()
     }
+    document.querySelector('.life span').innerText = gHearts;
 
     checkGameOver()
-
-    // if (gClicksTotal === 1) {
-    //     //the first cell click:
-    //     //setting it as empty cell-model+dom
-    //     //negs- empty/number
-    //     //setting level bombs in other places
-
-    // }
-
 }
+
 
 function handleRightClick(elCell, i, j, ev) {
     ev.preventDefault()
     cellMarked(elCell, i, j)
 }
 
+
 function cellMarked(elCell, i, j) {
-    // gClicksTotal++
-    // if()
+
+    if (gFlagsTotal === gLevel.MINES) return
+
+    gGame.markedCount++
+    if (gGame.shownCount === 0 && gGame.markedCount === 1) {
+        console.log('first right click');
+        startTimer()
+    }
+
     if (!gBoard[i][j].isMarked) {
         gBoard[i][j].isMarked = true
         gFlagsTotal++
@@ -113,22 +156,48 @@ function cellMarked(elCell, i, j) {
         gBoard[i][j].isMarked = false
         gFlagsTotal--
     }
-    document.querySelector('h3 span').innerText = gLevel.MINES - gFlagsTotal;
+    document.querySelector('.flag span').innerText = gLevel.MINES - gFlagsTotal;
     checkGameOver()
     // console.log('gFlagsTotal', gFlagsTotal);
     renderBoard(gBoard, '.board-container')
 }
 
 
-
+function restart() {
+    stopTimer()
+    init()
+}
 
 function checkGameOver() {
-    if (gFlagsTotal === gLevel.MINES && gClicksTotal === gLevel.SIZE ** 2 - gLevel.MINES) {
-        console.log('success! game over! you win!');
-        init()
+    if (gFlagsTotal === gLevel.MINES && checkIfIsEmpty(gBoard)) {
+        gIsVictory = true
+        winGame()
     }
 
 }
+
+function winGame() {
+    stopTimer()
+    console.log('success! game over! you win!');
+    document.querySelector('h4').classList.remove('hide');
+    document.querySelector('h4').innerText = 'You Win!';
+    gGame.isOn = false
+    document.querySelector('.restart span').innerText = '😎'
+
+}
+
+function loseGame() {
+    revealAllBombs(gBoard)
+    stopTimer()
+    console.log('You loose!!!');
+    document.querySelector('h4').classList.remove('hide');
+    document.querySelector('h4').innerText = 'You Loose!';
+    gGame.isOn = false
+    document.querySelector('.restart span').innerText = '😭'
+    // document.querySelector('.restart').classList.remove('hide');
+
+}
+
 
 
 function setMinesNegsCount(board) {
@@ -136,7 +205,6 @@ function setMinesNegsCount(board) {
     for (var i = 0; i < board.length; i++) {
         for (var j = 0; j < board[0].length; j++) {
             var countBombsNegs = countNeighbors(i, j, board)
-            // console.log(i, j, countBombsNegs);
             //model
             board[i][j].minesAroundCount = countBombsNegs
             //dom
--- js/util.js
+++ js/util.js
@@ -60,6 +60,8 @@ function renderBoard(mat, selector) {
     elContainer.innerHTML = strHTML;
 }
 
+
+
 // location such as: cell-i-j
 function renderCell(i, j, value) {
     // Select the elCell and set the value
@@ -69,6 +71,27 @@ function renderCell(i, j, value) {
 
 function changBgcToCell(i, j, color) {
     document.querySelector(`.cell-${i}-${j}`).style.backgroundColor = color
+}
+
+function revealAllBombs(mat) {
+    for (var i = 0; i < mat.length; i++) {
+        for (var j = 0; j < mat[0].length; j++) {
+            var elCell = mat[i][j]; //object
+            if (elCell.isMine) renderCell(i, j, MINE)
+        }
+    }
+}
+
+function checkIfIsEmpty(mat) {
+    var count = 0
+    for (var i = 0; i < mat.length; i++) {
+        for (var j = 0; j < mat[0].length; j++) {
+            var elCell = mat[i][j]; //object
+            if (elCell.isShown) count++
+        }
+    }
+    if (count === mat.length ** 2 - gLevel.MINES) return true
+    return false
 }
 
 //***********************RANDOM NUM/COLOR********************************** */
@@ -96,21 +119,21 @@ function countNeighbors(cellI, cellJ, mat) {
 }
 
 
-function randomBombs(board, size, numOfBombs) {
+function randomBombs(board, size, numOfBombs, i, j) {
     var countBombs = 0
     while (countBombs < numOfBombs) {
         var isValid = false
         while (!isValid) {
             var num1 = getRandomInt(0, size)
             var num2 = getRandomInt(0, size)
-            if (num1 !== num2) {
+            if ((num1 !== num2) && (num1 !== i && num2 !== j) && (num1 !== j && num2 !== i)) {
                 isValid = true
                 board[num1][num2].isMine = true
                 board[num2][num1].isMine = true
                 //the dom in the render
             }
         }
-        countBombs += 2
+        countBombs = countBombs + 2
     }
 }
 
@@ -124,17 +147,17 @@ function expandShown(mat, elCell, cellI, cellJ) {
             if (j < 0 || j >= mat[i].length) continue;
             if (i === cellI && j === cellJ) continue;
             gBoard[i][j].isShown = true
+            gGame.shownCount++
             gClicksTotal++
             renderBoard(gBoard, '.board-container')
         }
     }
+    return gGame.shownCount - 1
 }
 
 
 
-var gStartTime
-var gDiff
-var gCurrRecord
+
 //**********************TIMER************************************** */
 function startTimer() {
 
@@ -156,10 +179,13 @@ function updateTime() {
 }
 
 function stopTimer() {
-    gCurrRecord = gDiff
-    console.log('gCurrRecord', gCurrRecord);
-    document.querySelector('h1').innerText = 'Winner!';
-    document.querySelector('.btn-reset').classList.remove('hidden');
-    bestPlayer(gName, gCurrRecord)
+    if (gDiff < gCurrRecord && gIsVictory) {
+        gCurrRecord = gDiff
+        console.log('the best record!', gCurrRecord);
+
+    }
+    // document.querySelector('h1').innerText = 'Winner!';
+    // document.querySelector('.btn-reset').classList.remove('hidden');
+    // bestPlayer(gName, gCurrRecord)
     clearInterval(gInterval)
 }
